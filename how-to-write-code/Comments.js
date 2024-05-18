0c3226fcbd9bab48cd9238d7664609429db2cce4
Sofia Oguntolu <o.sofia@wustl.edu>
2024-05-18 17:15:23 -0500

    Comments.js done

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -4,9 +4,12 @@
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
-   /* Here is how
-   to have
-   multi-line
-   comments. */
+ /* I'm writing
+ my own
+ multi
+ line
+ comment */
 
    console.log("Comments make your code more readable by others.");
+
+// now i know how this works!
