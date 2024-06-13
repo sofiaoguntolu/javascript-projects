babfc71cf4905b6c0c3d3b003a603a9872efd456
Sofia Oguntolu <o.sofia@wustl.edu>
2024-06-13 12:14:51 -0500

    Done

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,12 @@
 // Declare and initialize the variables for exercise 1 here:
-
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let somputerStatusCode = 200;
+let shuttleSpeed = 15000;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+// I believe "engines are off" will be the printed message.
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,4 +1,4 @@
-console.log('Hello, JavaScript.');
+console.log("Hello, JavaScript.");
 console.log(2001);
 console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
