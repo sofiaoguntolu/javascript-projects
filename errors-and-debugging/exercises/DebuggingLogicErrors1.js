c76df717acbf2096de4ea19b4c23decc3793c2b9
Sofia Oguntolu <o.sofia@wustl.edu>
2024-06-13 13:12:44 -0500

    Done, i didn't realize we weren't supposed to figure it out on the first logic error file lol

--- errors-and-debugging/exercises/DebuggingLogicErrors1.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors1.js
@@ -10,19 +10,19 @@ let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
+   if (crewStatus && computerStatus === 'green'){
+      console.log('Crew & computer cleared.');
+      launchReady = true;
+   } else {
+      console.log('WARNING: Crew or computer not ready!');
+      launchReady = false;
+   }
    launchReady = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
    launchReady = false;
 }
 
-if (crewStatus && computerStatus === 'green'){
-   console.log('Crew & computer cleared.');
-   launchReady = true;
-} else {
-   console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
-}
 
 if (launchReady) {
    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
