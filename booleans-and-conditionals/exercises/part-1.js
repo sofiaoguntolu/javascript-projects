11157467ccf6cfba1a7d01ff5c3675cd5b642ae1
Sofia Oguntolu <o.sofia@wustl.edu>
2024-06-13 12:15:30 -0500

    Exercise done

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -3,7 +3,7 @@ let engineIndicatorLight = "red blinking";
 let spaceSuitsOn = true;
 let shuttleCabinReady = true;
 let crewStatus = spaceSuitsOn && shuttleCabinReady;
-let somputerStatusCode = 200;
+let computerStatusCode = 200;
 let shuttleSpeed = 15000;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 // I believe "engines are off" will be the printed message.
@@ -15,3 +15,30 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+//Safety Rules
+if (crewStatus){
+   console.log("Crew Ready");
+} else {
+   console.log("Crew Not Ready");
+}
+
+if (computerStatusCode === 200){
+   console.log("Please stand by. Computer is rebooting.");
+} else if (computerStatusCode === 400){
+   console.log("Success! Computer online.");
+} else{
+   console.log("ALERT: Computer offline!");
+}
+
+if (shuttleSpeed > 17500){
+   console.log("ALERT: Escape velocity reached!");
+} else if (shuttleSpeed < 8000){
+   console.log("ALERT: Cannot maintain orbit!");
+} else {
+   console.log("Stable speed");
+}
+
+//4. No
+
+//5:
